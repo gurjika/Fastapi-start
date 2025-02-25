9e4d3311db8bf77cde40eb602722837b7c722c68
Luka <lgurjidze@gmail.com>
2025-02-25 17:27:24 +0400

    add exceptions

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
+]
+
+
 @app.get('/')
-def read_root():
+async def read_root() ->  dict[str, str]:
     return {'message': 'Hello World'}
+
+
+@app.get('/bands')
+async def bands() -> list[dict]:
+    return BANDS
+
+@app.get('/bands/{id}')
+async def bands(id: int) -> dict:
+    for band in BANDS:
+        if band['id'] == id:
+            return band
+    raise HTTPException(status_code=404, detail='detail not found') 
+
+
+
+
